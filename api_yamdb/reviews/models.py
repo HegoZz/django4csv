f38f3ae89e6d234493b5f4dadc076dd8b2b7d00b
Андрей <HegoZ.oro@gmail.com>
2021-12-15 00:35:13 +0200

    add models categories, genres & titles

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class Categories(models.Model):
+    """Описание модели категории."""
+    name = models.CharField(verbose_name="Название категории", max_length=256)
+    slug = models.SlugField(
+        verbose_name="Уникальный идентификатор категории",
+        max_length=50,
+        unique=True
+    )
+
+
+class Genres(models.Model):
+    """Описание модели жанра."""
+    name = models.CharField(verbose_name="Название жанра", max_length=256)
+    slug = models.SlugField(
+        verbose_name="Уникальный идентификатор жанра",
+        max_length=50,
+        unique=True
+    )
+
+
+class Titles(models.Model):
+    name = models.CharField(verbose_name="Название жанра", max_length=256)
+    year = models.SmallIntegerField(verbose_name="Год выпуска")
+    rating = models.SmallIntegerField(verbose_name="Рейтинг", blank=True)
+    description = models.TextField(verbose_name="Описание", blank=True)
+    genre = models.ForeignKey(
+        Genres,
+        verbose_name='Жанры',
+        on_delete=models.DO_NOTHING,
+        related_name="titles",
+    )
+    category = models.ForeignKey(
+        Categories,
+        verbose_name='Категория',
+        on_delete=models.DO_NOTHING,
+        related_name="titles",
+    )
