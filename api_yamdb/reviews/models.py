1247d9102a8df65b006c21912bafad4a0e5bbcc7
Герман <QieDie@yandex.ru>
2021-12-15 10:10:47 +0300

    add models reviews and comment

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+
+SCORE = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10
+}
+
+
+class Review(models.Model):
+    """Модель для отзывов."""
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    score = models.CharField(max_length=1, choices=SCORE)
+    pub_date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    """Модель для комментариев к отзыву."""
+    reviews = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.text
