3347b4f816c948d2e62719970c234d9880986b2e
Андрей <HegoZ.oro@gmail.com>
2021-12-15 09:53:42 +0200

    add models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-<<<<<<< HEAD
 SCORE = {
     '1': 1,
     '2': 2,
@@ -56,44 +55,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-=======
-
-class Category(models.Model):
-    """Описание модели категории."""
-    name = models.CharField(verbose_name="Название категории", max_length=256)
-    slug = models.SlugField(
-        verbose_name="Уникальный идентификатор категории",
-        max_length=50,
-        unique=True
-    )
-
-
-class Genre(models.Model):
-    """Описание модели жанра."""
-    name = models.CharField(verbose_name="Название жанра", max_length=256)
-    slug = models.SlugField(
-        verbose_name="Уникальный идентификатор жанра",
-        max_length=50,
-        unique=True
-    )
-
-
-class Title(models.Model):
-    name = models.CharField(verbose_name="Название жанра", max_length=256)
-    year = models.SmallIntegerField(verbose_name="Год выпуска")
-    rating = models.SmallIntegerField(verbose_name="Рейтинг",
-                                      blank=True, null=True)
-    description = models.TextField(verbose_name="Описание", blank=True)
-    genre = models.ForeignKey(
-        Genre,
-        verbose_name='Жанры',
-        on_delete=models.DO_NOTHING,
-        related_name="titles",
-    )
-    category = models.ForeignKey(
-        Category,
-        verbose_name='Категория',
-        on_delete=models.DO_NOTHING,
-        related_name="titles",
-    )
->>>>>>> a49c2c40502896f430b639197ca6b6d20cb50158
